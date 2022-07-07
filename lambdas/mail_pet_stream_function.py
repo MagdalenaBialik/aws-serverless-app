d4a0ce47e98c064426b6088d0f46f0d08b26f71c
MagdalenaBialik <magdalena.bialik@gmail.com>
2022-07-07 20:34:18 +0200

    updated requirements.txt, added pre-commit file

--- lambdas/mail_pet_stream_function.py
+++ lambdas/mail_pet_stream_function.py
@@ -1,36 +1,26 @@
 import json
 import boto3
 
-ses_client = boto3.client('ses')
+ses_client = boto3.client("ses")
 
 
 def lambda_sent_pet(pet_of_the_day):
     ses_response = ses_client.send_email(
-        Source='alek.fidelus@gmail.com',
+        Source="alek.fidelus@gmail.com",
         Destination={
-            'ToAddresses': ['magdalena.bialik@gmail.com', 'alek.fidelus@gmail.com']
+            "ToAddresses": ["magdalena.bialik@gmail.com", "alek.fidelus@gmail.com"]
         },
         Message={
-            'Subject': {
-                'Data': 'Pet of the day'
-            },
-
-            'Body': {
-                'Text': {
-                    'Data': pet_of_the_day
-                }
-            }
-        }
+            "Subject": {"Data": "Pet of the day"},
+            "Body": {"Text": {"Data": pet_of_the_day}},
+        },
     )
     return ses_response
 
 
 def lambda_handler(event, context):
-    image = event['Records'][0]['dynamodb']['NewImage']
-    imie = image['PK']['S']
+    image = event["Records"][0]["dynamodb"]["NewImage"]
+    imie = image["PK"]["S"]
     ses_response = lambda_sent_pet(imie)
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps(ses_response)
-    }
+    return {"statusCode": 200, "body": json.dumps(ses_response)}
